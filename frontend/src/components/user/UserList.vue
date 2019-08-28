315a66e51ab126dd5d2f5ff8d9d87c3dd5931cd4
노현우 <wkdus7h@naver.com>
2019-08-28 06:26:47 +0000

    Update UserList.vue

--- frontend/src/components/user/UserList.vue
+++ frontend/src/components/user/UserList.vue
@@ -2,21 +2,8 @@
   <v-container grid-list-md text-center>
     <v-layout justify-center wrap>
       <v-flex xs7>
-        <h2>하위~</h2>
-
-        <v-layout column>
-          <v-flex v-for="card in userListCardsSliced" :key="card.id" pa-2>
-            <UserItem
-              :id="card.id"
-              :age="card.age"
-              :gender="card.gender"
-              :is_staff="card.is_staff"
-              :occupation="card.occupation"
-              :username="card.username"
-            />
-          </v-flex>
-          <v-pagination v-if="maxPages > 1" v-model="page" :length="maxPages" />
-        </v-layout>
+        <h2>유저 리스트</h2>
+        <UserItem :UserItems="userLists" />
       </v-flex>
     </v-layout>
   </v-container>
@@ -32,20 +19,7 @@ export default {
   },
   data: () => ({
     userLists: [],
-    cardsPerPage: 10,
-    page: 1
   }),
-  computed: {
-    userListEmpty: function() {
-      return this.userLists.length === 0;
-    },
-    maxPages: function() {
-      return Math.floor((this.userLists.length + this.cardsPerPage - 1) / this.cardsPerPage);
-    },
-    userListCardsSliced: function() {
-      return this.userLists.slice(this.cardsPerPage * (this.page - 1), this.cardsPerPage * this.page);
-    }
-  },
   mounted () {  
     this.getUserList();
   },
