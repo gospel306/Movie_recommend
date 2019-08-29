199a761a12882cc30807e9db1648bb99a115dd0a
josujang <whtnwkd@naver.com>
2019-08-29 17:47:19 +0900

    [조수장] 로그인, 회원가입 기능 구현

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import url
 from api.views import movie_views
 from api.views import auth_views
 from api.views import rating_views
+from api.views import signUp_views
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url('auth/signup-many/$', auth_views.signup_many, name='sign_up_many'),
     url('movies/$', movie_views.movies, name='movie_list'),
     url('ratings/$', rating_views.ratings, name='rating_list'),
-    url('rank/$', movie_views.rank, name='movie_list')
+    url('rank/$', movie_views.rank, name='movie_list'),
+    url('login/$', obtain_jwt_token),
+    url('signup/$',signUp_views.signup, name='sign_up_user'),
 ]
