4e44fb1504f84a409bda5333f50b55728baecece
김동욱 <gospel306@naver.com>
2019-09-04 15:24:07 +0900

    [동욱] pull and merge develop

---
+++ data/clustering.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+from matplotlib import colors as mcolors
+import math
+
+df = pd.read_csv('movieParsing.dat')
+X = np.array(df.drop(['MovieID'], 1).astype(float))
+X = preprocessing.scale(X)
+Y = np.array(df['MovieID'])
+
+clf = KMeans(n_clusters=10)
+clf.fit(X)
+print(clf.labels_)
+print(clf.labels_[0])
+
+
+
