8705549c632d37e94a9b0a85a0ebffdaa9711ac3
김동욱 <gospel306@naver.com>
2019-09-30 08:36:27 +0900

    [동욱]database update

--- backend/api/views/update_view.py
+++ backend/api/views/update_view.py
@@ -16,7 +16,7 @@ def update(request):
         ia = IMDb()
         for movie in movies:
             print(movie.title)
-            if movie.id < 32:
+            if movie.id < 1293:
                 continue
             try:
                 imovie = ia.search_movie(movie.title)
@@ -75,7 +75,6 @@ def update(request):
                 m = Movie.objects.get(pk=movie.id)
                 p = Person.objects.get(pk=cast.personID)
                 m.cast.add(p)
-
             for director in imovie['director']:
                 people = Person.objects.filter(pk=director.personID)
                 if not people:
@@ -110,37 +109,37 @@ def update(request):
                 m = Movie.objects.get(pk=movie.id)
                 p = Person.objects.get(pk=cast.personID)
                 m.director.add(p)
+            if 'writer' in imovie.keys():
+                for writer in imovie['writer']:
+                    people = Person.objects.filter(pk=writer.personID)
+                    if not people:
+                        person = ia.get_person(cast.personID)
+                        name = None
+                        height = None
+                        spouse = None
+                        biography = None
+                        birth_date = None
+                        birth_notes = None
+                        headshot = None
+                        if 'name' in person.keys():
+                            name = person['name']
+                        if 'spouse' in person.keys():
+                            spouse = '|'.join(person['spouse'])
+                        if 'height' in person.keys():
+                            height = person['height']
+                        if 'mini biography' in person.keys():
+                            biography = person['mini biography']
+                        if 'birth date' in person.keys():
+                            try:
+                                birth_date = datetime.strptime(person['birth date'], "%Y-%m-%d") + timedelta(hours=10)
+                            except:
+                                print("error")
+                        if 'birth notes' in person.keys():
+                            birth_notes = person['birth notes']
+                        if 'full-size headshot' in person.keys():
+                            headshot = person['full-size headshot']
 
-            for writer in imovie['writer']:
-                people = Person.objects.filter(pk=writer.personID)
-                if not people:
-                    person = ia.get_person(cast.personID)
-                    name = None
-                    height = None
-                    spouse = None
-                    biography = None
-                    birth_date = None
-                    birth_notes = None
-                    headshot = None
-                    if 'name' in person.keys():
-                        name = person['name']
-                    if 'spouse' in person.keys():
-                        spouse = '|'.join(person['spouse'])
-                    if 'height' in person.keys():
-                        height = person['height']
-                    if 'mini biography' in person.keys():
-                        biography = person['mini biography']
-                    if 'birth date' in person.keys():
-                        try:
-                            birth_date = datetime.strptime(person['birth date'], "%Y-%m-%d") + timedelta(hours=10)
-                        except:
-                            print("error")
-                    if 'birth notes' in person.keys():
-                        birth_notes = person['birth notes']
-                    if 'full-size headshot' in person.keys():
-                        headshot = person['full-size headshot']
-
-                    Person(id=cast.personID, name=name, height=height, spouse=spouse, biography=biography, birth_date=birth_date, birch_notes=birth_notes, headshot=headshot).save()
-                m = Movie.objects.get(pk=movie.id)
-                p = Person.objects.get(pk=cast.personID)
-                m.writer.add(p)
+                        Person(id=cast.personID, name=name, height=height, spouse=spouse, biography=biography, birth_date=birth_date, birch_notes=birth_notes, headshot=headshot).save()
+                    m = Movie.objects.get(pk=movie.id)
+                    p = Person.objects.get(pk=cast.personID)
+                    m.writer.add(p)
