961ffcdcdd445c88bbb4e48d18e32bd2da5571be
김경태 <kkt3526@naver.com>
2019-09-30 17:29:53 +0900

    [경태] 영화 상세정보

--- frontend/src/components/movie/MovieInfo.vue
+++ frontend/src/components/movie/MovieInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container column justify-content="space-between">
     <v-layout>
-      <v-flex xs8>
-        <h1>나쁜 녀석들</h1>
+      <v-flex xs9>
+        <h1>{{movie.title}}</h1>
         <v-flex>
           <v-layout>
             <h3>평점 :</h3>
@@ -20,39 +20,104 @@
         <v-flex>
           <v-layout>
             <h3>장르 :</h3>
-            {{genre}}
+            {{movie.genres}}
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <v-layout>
+            <h3>감독 :</h3>
+            <v-flex v-for="director in directorlist" :key="director.id"><a @click="PersonInfo(director.id)">{{director.name}}</a></v-flex>
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <v-layout>
+            <h3>작가 :</h3>
+            <v-flex v-for="writer in writerlist" :key="writer.id"><a @click="PersonInfo(writer.id)">{{writer.name}}</a></v-flex>
+          </v-layout>
+        </v-flex>
+
+        <v-flex>
+          <v-layout>
+            <h3>출연진 :</h3>
+          </v-layout>
+          <v-layout row>
+            <v-flex v-for="cast in castlist" :key="cast.id" pa-2><a @click="PersonInfo(cast.id)">{{cast.name}}</a></v-flex>
           </v-layout>
         </v-flex>
       </v-flex>
 
-      <v-flex xs4>
+      <v-flex xs3>
         <v-card
           :color="active ? undefined : 'grey lighten-1'"
           class="ma-4"
           height="400"
           width="300"
-        ></v-card>
+        >
+          <v-img :src="movie.poster" height="450"></v-img>
+        </v-card>
       </v-flex>
     </v-layout>
 
     <v-layout column>
       <h2>줄거리</h2>
-      {{content}}
+      {{movie.plot}}
+      <iframe
+        width="100%"
+        height="600"
+        :src="video"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </v-layout>
   </v-container>
 </template>
+
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      rating: 4,
-      genre: "범죄 / 액션",
-      content:
-        "블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라블라"
+      movie: {},
+      rating: 0,
+      video: "https://www.youtube.com/embed/",
+      directorlist: [],
+      writerlist: [],
+      castlist: []
     };
   },
-  mounted(){
-    alert(this.$route.params.id);
+  mounted() {
+    this.MovieInfomation(this.$route.params.id);
+    this.rating = this.$route.params.rating;
+  },
+  methods: {
+    MovieInfomation(id) {
+      axios
+        .get(this.$store.state.server + "/api/moviedetail/?id=" + id)
+        .then(res => {
+          this.movie = res.data[0];
+          this.PeopleData();
+        });
+    },
+    PeopleData() {
+      for (var i = 0; i < this.movie.directorlist.length; i++) {
+        var d = this.movie.directorlist[i].split(":");
+        this.directorlist.push({ id: d[0], name: d[1] });
+      }
+      for (var j = 0; j < this.movie.writerlist.length; j++) {
+        var w = this.movie.writerlist[j].split(":");
+        this.writerlist.push({ id: w[0], name: w[1] });
+      }
+      for (var k = 0; k < this.movie.castlist.length; k++) {
+        var c = this.movie.castlist[k].split(":");
+        this.castlist.push({ id: c[0], name: c[1] });
+      }
+    },
+    PersonInfo(id){
+      alert(id);
+    }
   }
 };
 </script>
