444fc01ca4f709f22de670ab6659f0599e7bf5d2
josujang <whtnwkd@naver.com>
2019-10-07 15:02:28 +0900

    [수장] nav 바 수정
    Former-commit-id: 51339110a1584a678f7c6e25b1ab0b6f238af3e6

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -7,16 +7,12 @@
         </v-flex>
       </v-layout>
     </v-container>
-    <LeftNav class="nav"></LeftNav>
-    <RightNav class="nav"></RightNav>
+    <LeftNav class="ck"></LeftNav>
+    <RightNav></RightNav>
   </v-content>
 </template>
 
 <script>
-/*
-    <LeftNav class="nav"></LeftNav>
-    <RightNav class="nav"></RightNav>
-*/
 import LeftNav from "@/components/common/LeftNav";
 import RightNav from "@/components/common/RightNav";
 
@@ -43,17 +39,9 @@ export default {
     handleResize() {
       this.window.width = window.innerWidth;
       this.window.height = window.innerHeight;
-      var leftNum = this.window.height / 49 - this.$store.state.leftNum;
-      var rightNum = this.window.height / 49 - this.$store.state.rightNum;
-      this.$store.state.leftTemp.length = 0;
-      this.$store.state.rightTemp.length = 0;
-
-      for(var i = 1; i <= leftNum; i++){
-        this.$store.state.leftTemp.push(i);
-      };
-      for(var i = 1; i <= rightNum; i++){
-        this.$store.state.rightTemp.push(i);
-      };
+      var num = window.innerHeight / 50;
+      this.$store.state.leftNavNum = Math.floor(num-3);
+      this.$store.state.rightNavNum = Math.floor(num-2);
     }
   },
 };
@@ -63,25 +51,18 @@ export default {
 .back{
   background-color : black;
   }
-  /*
-    border-width: 1px;
-  border-color: blue;
-  border-style : solid;
-
-    border-width: 5px;
-  border-color: aqua;
-  border-style : solid;
-  margin : 10px;
-
-   */
 .main-board{
   background-color: white;
-  border-radius: 30px;
+  border-radius: 15px;
   height: 85%;
   display: flex;
   align-items: center;
   justify-content: center;
 }
-
+.ck{
+  border-color: aqua;
+  border-width:3px;
+  border-style: solid;
+}
 </style>
 
